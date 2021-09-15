e557673afb8c1252297a27e799a8cba474e7e157
DavideZattra <d.zattra@gmail.com>
2021-09-15 16:24:36 +0200

    added nav function

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -4,13 +4,34 @@ var app = new Vue(
 
 		data : {
 			images: [
-				{url : 'img/puppie-1.jpg', alt : 'Border Collie Puppy'},
-				{url : 'img/puppie-2.jpg', alt : 'Labrador Chocolate Puppy'},
-				{url : 'img/puppie-3.jpg', alt : 'Puppy'},
-				{url : 'img/puppie-4.jpg', alt : 'Labrador Chocolate Puppy'},
-				{url : 'img/puppie-5.jpg', alt : 'Husky Puppy'},
-				{url : 'img/puppie-6.jpg', alt : 'Husky Puppy'},
-				{url : 'img/golden_lab_poo.jpeg', alt : 'Labrador Golden Puppy'},			],	
+				{
+					url : 'img/puppie-1.jpg', 
+					alt : 'Border Collie Puppy'
+				},
+				{
+					url : 'img/puppie-2.jpg', 
+					alt : 'Labrador Chocolate Puppy'},
+				{
+					url : 'img/puppie-3.jpg', 
+					alt : 'Puppy'
+				},
+				{
+					url : 'img/puppie-4.jpg', 
+					alt : 'Labrador Chocolate Puppy'
+				},
+				{
+					url : 'img/puppie-5.jpg', 
+					alt : 'Husky Puppy'
+				},
+				{
+					url : 'img/puppie-6.jpg', 
+					alt : 'Husky Puppy'
+				},
+				
+					{url : 'img/golden_lab_poo.jpeg', 
+					alt : 'Labrador Golden Puppy'
+				},			
+			],	
 
 			imgIndex : 0,
 		},
@@ -34,8 +55,12 @@ var app = new Vue(
 					this.imgIndex = 0;
 				}
 				console.log(this.imgIndex);
-			}
+			},
+			
+			navImg : function(imgIndex){
+				this.imgIndex = imgIndex;
 				
+			}
 		},
 
 	}
