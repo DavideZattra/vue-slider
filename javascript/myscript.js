24e99e673563347a04cb13ae5ae1287062b67895
DavideZattra <d.zattra@gmail.com>
2021-09-15 15:19:42 +0200

    created layout and navigation dot

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -2,7 +2,20 @@ var app = new Vue(
 	{
 		el : '#app',
 		data : {
-						
-		}
+			images: [
+				{src : 'https://www.terranuova.it/var/terranuova/storage/images/news/attualita/cani-ecco-come-i-cuccioli-imparano-dall-uomo/1338268-1-ita-IT/Cani-ecco-come-i-cuccioli-imparano-dall-uomo_articleimage.jpg', alt : ''},
+				{src : 'https://lh3.googleusercontent.com/proxy/SgMrp8CZm9ExCy9hbi9aS_WXCPtE6ZYzLajLDS6xO1vKthvWLqIWSKrBpL5A8G5fWSKC_usvE4qinLtxAQ5Y3fhJzgu4w36n2831RODD5KpkirK1iO42m7-p_DJKayHNtopIx0ueR-WreYncCRZN7_tzFhJgDeDwsA7aWW8MLrm1YKduog', alt : ''},
+				{src : '', alt : ''},
+				{src : '', alt : ''},
+				{src : '', alt : ''},
+				{src : '', alt : ''}
+			],		
+			imgIndex : 0
+		},
+
+		methods: {
+			
+		},
+
 	}
 );
